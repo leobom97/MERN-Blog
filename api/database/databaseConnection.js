955f47ae95359ce81a4509de12ad1fca00de6990
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-02-24 19:51:10 -0300

    database connection

--- api/database/databaseConnection.js
+++ api/database/databaseConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const databaseConnection = async () => {
   try {
     const conexao = await mongoose.connect(
-      "mongodb+srv://doriacruz2021:8f9b61123@cluster0.yiclkpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://doriacruz2021:8f9b61123@leos-mern-blog.5g3qxte.mongodb.net/?retryWrites=true&w=majority&appName=LEOS-MERN-BLOG"
     );
     console.log(`Database succefully connected: ${conexao.connection.host}`);
   } catch (error) {
