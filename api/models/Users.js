15192ca6e7c0e018e43d31d4c6a0e49ffa6e290a
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-02-24 20:08:29 -0300

    UserModel update

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,28 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-  },
-  posts: {
-    type: Array,
-  },
-});
+  { timestamps: true }
+);
 
-const userModel = mongoose.model("UserModel", userSchema);
+const User = mongoose.model("User", userSchema);
 
-export default userModel;
+export default User;
