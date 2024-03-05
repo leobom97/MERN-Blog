9b6416e290009cb49df2e2f8875c8db2e5b700db
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-03-05 17:56:50 -0300

    User delete api route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateUser, test } from "../controllers/userController.js";
+import { updateUser, test, deleteUser } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
@@ -7,5 +7,6 @@ const userRouter = express.Router();
 //endpoint
 userRouter.get("/", test);
 userRouter.put("/update/:userId", verifyToken, updateUser);
+userRouter.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default userRouter;
