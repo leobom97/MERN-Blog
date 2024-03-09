4b16f58633ed8656379c2d97e1c4333048a95265
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-03-08 23:26:50 -0300

    delete post funcionality on the dashboard

--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create, getPosts } from "../controllers/postController.js";
+import { create, deletePost, getPosts } from "../controllers/postController.js";
 const postRouter = express.Router();
 
 postRouter.post("/create", verifyToken, create);
 postRouter.get("/getposts", getPosts);
+postRouter.delete("/delete/:postId/:userId", verifyToken, deletePost);
 export default postRouter;
