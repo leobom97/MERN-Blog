82b2b30db36d3a4edac7168273a0276a471f1b98
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-03-10 14:55:00 -0300

    create get users api route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   test,
   deleteUser,
   signout,
+  getUsers,
 } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -14,5 +15,6 @@ userRouter.get("/test", test);
 userRouter.put("/update/:userId", verifyToken, updateUser);
 userRouter.delete("/delete/:userId", verifyToken, deleteUser);
 userRouter.post("/signout", signout);
+userRouter.get("/getusers", verifyToken, getUsers);
 
 export default userRouter;
