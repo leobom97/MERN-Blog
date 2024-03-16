e6a5ac2d1fdd1affe7822f73fe25e0f9d2a718d1
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-03-15 21:37:26 -0300

    Complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Leonardo's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu
+              non sodales neque sodales ut. Cras pulvinar mattis nunc sed
+              blandit libero volutpat. Mi tempus imperdiet nulla malesuada
+              pellentesque. Purus semper eget duis at tellus at urna. Eleifend
+              mi in nulla posuere. Eu ultrices vitae auctor eu augue ut lectus
+              arcu. Tincidunt arcu non sodales neque sodales ut etiam sit amet.
+              Sodales ut eu sem integer vitae. Quis imperdiet massa tincidunt
+              nunc pulvinar sapien. Tortor at risus viverra adipiscing at in. Et
+              ligula ullamcorper malesuada proin libero. Sit amet consectetur
+              adipiscing elit ut. Blandit volutpat maecenas volutpat blandit.
+            </p>
 
-function About() {
-  return <div>About</div>;
+            <p>
+              Placerat in egestas erat imperdiet. Aliquet porttitor lacus luctus
+              accumsan tortor. Neque gravida in fermentum et sollicitudin ac.
+              Aliquam sem et tortor consequat. Nisi quis eleifend quam
+              adipiscing vitae. Ut pharetra sit amet aliquam id diam maecenas.
+              Id volutpat lacus laoreet non curabitur gravida arcu ac. Sed
+              ullamcorper morbi tincidunt ornare. Metus aliquam eleifend mi in
+              nulla. Semper quis lectus nulla at volutpat. Hendrerit gravida
+              rutrum quisque non tellus orci ac auctor augue. Velit egestas dui
+              id ornare arcu odio ut sem.
+            </p>
+
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus. Quam
+              lacus suspendisse faucibus interdum. Volutpat diam ut venenatis
+              tellus in metus vulputate. Nunc id cursus metus aliquam. Ornare
+              arcu odio ut sem nulla pharetra. Leo vel fringilla est
+              ullamcorper. Venenatis cras sed felis eget velit aliquet sagittis.
+              Tellus integer feugiat scelerisque varius morbi. A arcu cursus
+              vitae congue mauris rhoncus aenean. Egestas maecenas pharetra
+              convallis posuere morbi leo urna molestie.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default About;
