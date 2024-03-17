5862d03724eb88a9ecf45cf3062764b5c20f7e61
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-03-17 16:47:31 -0300

    Removal Calltoaction component import and remaining selectors

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Calltoaction from "../components/Calltoaction";
 import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
 
