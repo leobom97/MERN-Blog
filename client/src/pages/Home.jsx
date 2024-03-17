5fcc3605b261a89936f884464a01f3d70356d966
Leonardo Rodrigues <leordoria2016@gmail.com>
2024-03-17 16:34:52 -0300

    calltoaction component removed from the Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,10 +29,6 @@ export default function Home() {
           View all posts
         </Link>
       </div>
-      <div className="p-3 bg-amber-100 dark:bg-slate-700">
-        <Calltoaction />
-      </div>
-
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
           <div className="flex flex-col gap-6">
